bd9df1b627d5fa4a949abe994b56242ab09610d8
ムル <110756470+mulgsx@users.noreply.github.com>
2026-01-19 19:31:27 +0900

    ios and fix SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -341,13 +341,15 @@ class GT7RpmAppState extends State<GT7RpmApp> {
         title: const Text("GT7 RPM Tracker"),
 
         // 2. Menu button on the left (Leading)
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              // 3. Open the drawer
-              Scaffold.of(context).openDrawer();
-            },
+        leading: SafeArea(
+          child: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                // 3. Open the drawer
+                Scaffold.of(context).openDrawer();
+              },
+            ),
           ),
         ),
       ),
